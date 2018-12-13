c62f1a9f6e1964c5e2859bf5c3cfce13cb581ee2
Dusan Milovic <Dusan95kv@gmail.com>
2018-12-13 15:41:53 +0100

    Add files via upload

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -34,8 +34,8 @@ end@
 -- 			ZAHTEVA ODMOR
 -- =======================
 
-drop trigger if exists ai_zahtevaOdmor@
-create trigger ai_zahtevaOdmor after insert on zahtevaOdmor
+drop trigger if exists ai_odmor@
+create trigger ai_odmor after insert on odmor
 for each row
 begin
 	declare novi_broj_slobodnih_dana int;
@@ -48,7 +48,7 @@ begin
     declare ima_odgovoren_posle_novog_dana int;
     
     set ima_zahtev_pre_novog_dana = (
-														  select count(*) from zahtevaOdmor zo
+														  select count(*) from odmor zo
                                                           where zo.korisnik_email = new.korisnik_email and
                                                           zo.deleted = 0 and
                                                           zo.dan_odmora = DATE_ADD(new.dan_odmora, interval -1 day)
@@ -56,14 +56,14 @@ begin
                                                         
 	set ima_odgovoren_pre_novog_dana = (
 																	select count(*) from odgovorNaZahtevOdmora onzo
-                                                                    where onzo.zahtevaOdmor_korisnik_email = new.korisnik_email and
+                                                                    where onzo.odmor_korisnik_email = new.korisnik_email and
                                                                     (onzo.status = 'odobren' or onzo.status = 'na cekanju') and 
-                                                                    onzo.zahtevaOdmor_dan_odmora = DATE_ADD(new.dan_odmora, interval -1 day)
+                                                                    onzo.odmor_dan_odmora = DATE_ADD(new.dan_odmora, interval -1 day)
 																	);
                                                         
                                                         
       set ima_zahtev_posle_novog_dana = (
-														  select count(*) from zahtevaOdmor zo
+														  select count(*) from odmor zo
                                                           where zo.korisnik_email = new.korisnik_email and
                                                           zo.deleted = 0 and
                                                           zo.dan_odmora = DATE_ADD(new.dan_odmora, interval 1 day)
@@ -71,9 +71,9 @@ begin
                                                         
 	set ima_odgovoren_posle_novog_dana = (
 																		select count(*) from odgovorNaZahtevOdmora onzo
-																		where onzo.zahtevaOdmor_korisnik_email = new.korisnik_email and
+																		where onzo.odmor_korisnik_email = new.korisnik_email and
 																		(onzo.status = 'odobren' or onzo.status = 'na cekanju') and 
-																		onzo.zahtevaOdmor_dan_odmora = DATE_ADD(new.dan_odmora, interval 1 day)
+																		onzo.odmor_dan_odmora = DATE_ADD(new.dan_odmora, interval 1 day)
 																		);
                                                                         
 	if (ima_zahtev_pre_novog_dana > 0 or ima_odgovoren_pre_novog_dana > 0 or ima_zahtev_posle_novog_dana > 0 or ima_odgovoren_posle_novog_dana > 0)
@@ -109,8 +109,8 @@ begin
     end if;
     
     -- moze da odgovara samo na !delete zahteve
-    set check_if_is_deleted = (select deleted from zahtevaOdmor zo
-											   where zo.korisnik_email = new.zahtevaOdmor_korisnik_email and zo.dan_odmora = new.zahtevaOdmor_dan_odmora
+    set check_if_is_deleted = (select deleted from odmor zo
+											   where zo.korisnik_email = new.odmor_korisnik_email and zo.dan_odmora = new.odmor_dan_odmora
 											  );
     if (check_if_is_deleted = 1)
 		then signal sqlstate '45000' set message_text='Ne moze da odgovori na obrisani zahtev za odmor';
@@ -121,7 +121,7 @@ begin
 	then begin
     set odmori_u_istom_danu = (select count(*) from odgovorNaZahtevOdmora
 												  where status like 'odobren' and 
-                                                  zahtevaOdmor_dan_odmora = new.zahtevaOdmor_dan_odmora
+                                                  odmor_dan_odmora = new.odmor_dan_odmora
 												);
 	set ukupno_ljudi = (select count(*) from korisnik);
 	set procenat_ljudi_na_odmoru = odmori_u_istom_danu *100 / ukupno_ljudi;
@@ -143,27 +143,27 @@ begin
     
 	if (new.status like 'odobren')
 	then begin
-		set razlog_promene_dana = concat('Odobren zahtev za odmor na dan: ', new.zahtevaOdmor_dan_odmora);
-		set novi_broj_potvrdjeni_dani = (select potvrdjeni_dani from aktivnaStatistikaUsera asu where asu.korisnik_email = new.zahtevaOdmor_korisnik_email) + 1;
-		set novi_broj_dana_na_cekanju = (select dani_na_cekanju from aktivnaStatistikaUsera asu where asu.korisnik_email = new.zahtevaOdmor_korisnik_email) - 1;
+		set razlog_promene_dana = concat('Odobren zahtev za odmor na dan: ', new.odmor_dan_odmora);
+		set novi_broj_potvrdjeni_dani = (select potvrdjeni_dani from aktivnaStatistikaUsera asu where asu.korisnik_email = new.odmor_korisnik_email) + 1;
+		set novi_broj_dana_na_cekanju = (select dani_na_cekanju from aktivnaStatistikaUsera asu where asu.korisnik_email = new.odmor_korisnik_email) - 1;
     
-		update aktivnaStatistikaUsera set potvrdjeni_dani = novi_broj_potvrdjeni_dani, dani_na_cekanju = novi_broj_dana_na_cekanju where korisnik_email = new.zahtevaOdmor_korisnik_email;
-        insert into pracenjeDana values (YEAR(new.zahtevaOdmor_dan_odmora), razlog_promene_dana, 1, null,  new.zahtevaOdmor_korisnik_email);
+		update aktivnaStatistikaUsera set potvrdjeni_dani = novi_broj_potvrdjeni_dani, dani_na_cekanju = novi_broj_dana_na_cekanju where korisnik_email = new.odmor_korisnik_email;
+        insert into pracenjeDana values (YEAR(new.odmor_dan_odmora), razlog_promene_dana, 1, null,  new.odmor_korisnik_email);
 	end;
 	end if;
    
 	if (new.status like 'odbijen')
 	then begin
-		set novi_broj_preostalo_slobodnih_dana = (select preostalo_slobodnih_dana from aktivnaStatistikaUsera asu where asu.korisnik_email = new.zahtevaOdmor_korisnik_email) + 1;
-		set novi_broj_dana_na_cekanju = (select dani_na_cekanju from aktivnaStatistikaUsera asu where asu.korisnik_email = new.zahtevaOdmor_korisnik_email) - 1;
+		set novi_broj_preostalo_slobodnih_dana = (select preostalo_slobodnih_dana from aktivnaStatistikaUsera asu where asu.korisnik_email = new.odmor_korisnik_email) + 1;
+		set novi_broj_dana_na_cekanju = (select dani_na_cekanju from aktivnaStatistikaUsera asu where asu.korisnik_email = new.odmor_korisnik_email) - 1;
     
-		update aktivnaStatistikaUsera set preostalo_slobodnih_dana = novi_broj_preostalo_slobodnih_dana, dani_na_cekanju = novi_broj_dana_na_cekanju where korisnik_email = new.zahtevaOdmor_korisnik_email;
+		update aktivnaStatistikaUsera set preostalo_slobodnih_dana = novi_broj_preostalo_slobodnih_dana, dani_na_cekanju = novi_broj_dana_na_cekanju where korisnik_email = new.odmor_korisnik_email;
 	end;
 	end if;
    
 	if (new.status like 'odobren' or new.status like 'odbijen')
 	then begin
-		update zahtevaOdmor set deleted = 1 where new.zahtevaOdmor_dan_odmora = dan_odmora and new.zahtevaOdmor_korisnik_email = korisnik_email;
+		update odmor set deleted = 1 where new.odmor_dan_odmora = dan_odmora and new.odmor_korisnik_email = korisnik_email;
 	end;
 	end if;
 end@
